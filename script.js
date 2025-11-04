0873c08a60ca6da165df82f47c226330adcb10de
Felipesantos66 <f74702965@gmail.com>
2025-11-03 21:23:47 -0300

    .

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+
+hamburger.addEventListener('click', () => {
+  const expanded = hamburger.getAttribute('aria-expanded') === 'true';
+  hamburger.setAttribute('aria-expanded', !expanded);
+  navLinks.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
+
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+    hamburger.classList.remove('active');
+    hamburger.setAttribute('aria-expanded', false);
+  });
+});
